07e9ee756622640dc00ef84f965f303ca5ced59d
MrChild-MrAdult <mr.child0729@gmail.com>
2020-10-12 17:29:12 +0900

    commentout assosiation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  belongs_to :category, dependent: :destroy
-  belongs_to :brand, dependent: :destroy
+  # belongs_to :user, dependent: :destroy
+  # belongs_to :category, dependent: :destroy
+  # belongs_to :brand, dependent: :destroy
   has_many :images, dependent: :destroy
 end
