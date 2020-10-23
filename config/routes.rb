3d697bf85af6d586055c981b35fc892cd747dac0
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2020-10-23 22:01:36 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :products, only: [:index, :search, :show, :new, :create, :edit, :update] do
+  resources :products, only: [:index, :show, :new, :create, :edit, :update] do
     collection do
+      get :search
       get :pay
       get :check
     end
