b64de930b667fa778451a2c03da86e3188e7a34e
MrChild-MrAdult <mr.child0729@gmail.com>
2020-12-09 11:46:51 +0900

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
-  before_action :set_product, only: [:show, :destroy, :edit, :update]
+  before_action :set_product, only: [:show, :destroy, :edit, :update, :buy]
+
+  require "payjp"
 
   def index
     @products = Product.includes(:images).order('created_at DESC')
@@ -48,9 +50,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  def check
-  end
-
   def search
     @search = Product.ransack(params[:q]) 
     @search_products = @search.result
@@ -71,6 +70,43 @@ class ProductsController < ApplicationController
       format.json do
         @category_grandchildren = Category.find("#{params[:child_id]}").children
       end
+    end
+  end
+  
+
+
+  def buy
+    # 商品ごとに複数枚写真を登録できるので、一応全部持ってきておきます。
+    # @images = @product.images.all
+
+    # まずはログインしているか確認
+    if user_signed_in?
+      @user = current_user
+      # クレジットカードが登録されているか確認
+      if @user.credit_card.present?
+        # 前前前回credentials.yml.encに記載したAPI秘密鍵を呼び出します。
+        Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+        # ログインユーザーのクレジットカード情報を引っ張ってきます。
+        @card = CreditCard.find_by(user_id: current_user.id)
+        # (以下は以前のcredit_cardsコントローラーのshowアクションとほぼ一緒です)
+        # ログインユーザーのクレジットカード情報からPay.jpに登録されているカスタマー情報を引き出す
+        customer = Payjp::Customer.retrieve(@card.customer_id)
+        # カスタマー情報からカードの情報を引き出す
+        @customer_card = customer.cards.retrieve(@card.card_id)
+
+        ##カードのアイコン表示のための定義づけ
+        @card_brand = @customer_card.brand
+
+        # viewの記述を簡略化
+        ## 有効期限'月'を定義
+        @exp_month = @customer_card.exp_month.to_s
+        ## 有効期限'年'を定義
+        @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+      else
+      end
+    else
+      # ログインしていなければ、商品の購入ができずに、ログイン画面に移動します。
+      redirect_to root_path, alert: "ログインしてください"
     end
   end
 
