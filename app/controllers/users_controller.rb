76d2e1bc9b55358aeb5dc00e31c84431860ab6b5
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2020-12-16 16:52:59 +0900

    soldout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,33 @@
 class UsersController < ApplicationController
 
+
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
     @products = user.products
+    @introduction = user.introduction
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user), notice: '変更されました'
   end
 
   def logout
   end
 
   def personal_edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:introduction)
   end
 
 end
